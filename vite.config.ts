e62d74a95db1444e19492b9215d1778eb22f7280
Lucas79DIas <lucasaugustodias79@gmail.com>
2026-02-24 18:39:41 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
+  root: "client",
   plugins: [react()],
-  base: "./"
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "client/src"),
+    },
+  },
+  build: {
+    outDir: "dist/client",
+    emptyOutDir: true,
+  },
 });
