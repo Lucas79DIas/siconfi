a6162e01ddd01f1d27649f01c82e9dcdf2de0e78
Lucas79DIas <lucasaugustodias79@gmail.com>
2026-02-25 10:14:47 -0300

    Refactor App component structure
    Refactor App component to remove routing and add a simple greeting.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,12 @@
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/NotFound";
-import { Route, Switch } from "wouter";
-import ErrorBoundary from "./components/ErrorBoundary";
-import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
-import Consolidacao from "./pages/Consolidacao";
+import React from "react";
 
-function Router() {
+const App: React.FC = () => {
   return (
-    <Switch>
-      <Route path={"/"} component={Home} />
-      <Route path={"/consolidacao"} component={Consolidacao} />
-      <Route path={"/404"} component={NotFound} />
-      <Route component={NotFound} />
-    </Switch>
+    <div>
+      <h1>Olá, Siconfi!</h1>
+      <p>Aplicação mínima funcionando no Vercel Serverless.</p>
+    </div>
   );
-}
-
-function App() {
-  return (
-    <ErrorBoundary>
-      <ThemeProvider defaultTheme="light">
-        <TooltipProvider>
-          {/* <Toaster /> Temporariamente comentado para build */}
-          <Router />
-        </TooltipProvider>
-      </ThemeProvider>
-    </ErrorBoundary>
-  );
-}
+};
 
 export default App;
